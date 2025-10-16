ed8a3326abe2994359399779f3d49e30a87a7e83
AbhinavKV369 <abhinavkvmayyil@gmail.com>
2025-10-16 19:32:52 +0530

    Added signin and signout functions

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,4 +1,5 @@
 import User from "../models/User.js";
+import jwt from "jsonwebtoken";
 import { signinSchema, signupSchema } from "../middlewares/validator.js";
 import { doHash, doHashValidation } from "../utils/hashing.js";
 
@@ -45,23 +46,34 @@ export const postSignin = async (req, res) => {
         .json({ success: false, message: error.details[0].message });
       console.log(error);
     }
-    89
-    const existingUser = await User.findOne({ email }).select('password');
+    const existingUser = await User.findOne({ email }).select("password");
     if (!existingUser) {
       res.status(401).json({ success: false, message: "Invalid Credentials" });
     }
-    const result = await doHashValidation(password,existingUser.password);
+    const result = await doHashValidation(password, existingUser.password);
 
-    if(!result){
+    if (!result) {
       res.status(401).json({
-        success:false,
-        message: "Invalid Credential"
+        success: false,
+        message: "Invalid Credential",
       });
     }
-
-    res.status(201).json({success:true,message:"Signin Successfull"})
+    const payload = {
+      user: {
+        id: existingUser._id,
+      },
+    };
+    const secret = process.env.JWT_SECRET;
+    const token = jwt.sign(payload, secret);
+    res.cookie("Authorization","Bearer" +token,{expires: new Date(Date.now()+8 + 60*60*1000),httpOnly:process.env.NODE_ENV ==="production"}).json({ success: true, message: "Signin Successfull" });
   } catch (error) {
     console.log(error);
     res.status(500).json({ success: false, message: `Signup Failed ${error}` });
   }
 };
+
+export const signout = async(req,res) =>{
+  res.clearCookie("Authorization")
+  .status(200)
+  .json({success:true,message:"Signout Successfull"})
+}
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,12 @@
 import express from "express"
 
-import { postSignin, postSignup } from "../controllers/authControllers.js"
+import { postSignin, postSignup, signout } from "../controllers/authControllers.js"
 
 
 const router = express.Router();
 
 router.post("/signup",postSignup);
-router.post("/signin",postSignin)
+router.post("/signin",postSignin);
+router.post("/signout",signout);
 
 export default router;
